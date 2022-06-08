c45de175215e11ff8f097567fed73c0e953162ca
sharmaarnav <007.arnav@gmail.com>
2022-06-08 13:31:20 +1000

    df

---
+++ Modules/AppServicePlan/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.8.0"
+    }
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+
+}
