77a84757912d6b1c3347bb91c461814fe0fbc0f6
sharmaarnav <007.arnav@gmail.com>
2022-06-09 13:54:59 +1000

    ds

--- Modules/VirtualNetwork/main.tf
+++ Modules/VirtualNetwork/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "example" {
 
 resource "azurerm_subnet" "example" {
   name                 = "${var.virtual_network_name}-subnet01"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
